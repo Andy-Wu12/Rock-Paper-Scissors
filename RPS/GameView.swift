a0397282f9c9f0eb62ffc748112a39a4c46bb043
Andy Wu <awpats12@gmail.com>
2022-12-31 21:29:56 -0500

    Add win checking, result message, socre updates

--- RPS/GameView.swift
+++ RPS/GameView.swift
@@ -13,19 +13,36 @@ struct GameView: View {
     @State private var userMadeChoice: Bool = false
     @State private var cpuScore = 0
     @State private var playerScore = 0
+    @State private var resultText = ""
     
     var body: some View {
         VStack {
             // Scoreboard
             Scoreboard(leftName: "CPU", leftScore: cpuScore, rightName: "Player", rightScore: playerScore)
+                .padding(.top)
             // CPU
             Spacer()
-            OpponentView(revealed: userMadeChoice)
+            OpponentView(revealed: $userMadeChoice)
+            Spacer()
+            Text(resultText)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
             Spacer()
             // Player
             HStack {
                 ForEach(Array(options.keys), id: \.self) { optionKey in
-                    Button(action: {}) {
+                    Button(action: {
+                        userChoice = optionKey
+                        userMadeChoice = true
+                        
+                        // Generate random choice for cpu
+                        generateCPUChoice()
+                        
+                        // Check result
+                        let result = checkWin(playerChoice: userChoice, cpuChoice: cpuChoice)
+                        updateResult(result: result)
+                        
+                    }) {
                         VStack {
                             Text(options[optionKey]!)
                             Text(optionKey)
@@ -38,17 +55,68 @@ struct GameView: View {
             Spacer()
         }
     }
+
+    func getResultText(result: Int) -> String {
+        switch result {
+        case -1:
+            return "Lose"
+        case 0:
+            return "Tie"
+        case 1:
+            return "Win"
+        default:
+            return "Error"
+        }
+    }
+    
+    // -1: lose, 0: draw, 1: win
+    func checkWin(playerChoice: String, cpuChoice: String) -> Int {
+        var result = 0
+        
+        if playerChoice == cpuChoice {
+            result = 0
+        } else if playerChoice == "rock" {
+            if cpuChoice == "scissors" { result = 1 }
+            else { result = -1 }
+        } else if playerChoice == "paper" {
+            if cpuChoice == "rock" { result = 1 }
+            else { result = -1 }
+        } else if playerChoice == "scissors" {
+            if cpuChoice == "paper" { result = 1 }
+            else { result = -1 }
+        }
+        return result
+    }
+    
+    func generateCPUChoice() {
+        cpuChoice = ["rock", "paper", "scissors"].randomElement()!
+        
+    }
+    
+    func updateResult(result: Int) {
+        resultText = getResultText(result: result)
+        if result == 1 {
+            playerScore += 1
+        } else if result == -1 {
+            cpuScore += 1
+        }
+    }
 }
 
 struct OpponentView: View {
     @State private var choice: String = options.randomElement()!.key
-    @State var revealed: Bool
+    @Binding var revealed: Bool
     
     var body: some View {
         Group {
             if revealed {
-                Text("Your opponent chose \(options[choice]!)")
-                    .font(.system(size: 40))
+                VStack {
+                    Text("Opponent chose ")
+                        .font(.system(size: 40))
+                    Text(options[choice]!)
+                        .iconStyle()
+                }
+                
             } else {
                 Text("Waiting for you to choose")
                     .font(.system(size: 25))
