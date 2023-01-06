1892e1d3f13f89e249db3a9043613b591a46b20b
Andy Wu <awpats12@gmail.com>
2023-01-06 01:10:10 -0500

    Improved StatView's UI in landscape mode

--- RPS/Views/StatView.swift
+++ RPS/Views/StatView.swift
@@ -13,34 +13,33 @@ struct StatView: View {
     @Binding var showingStats: Bool
     
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView {
-                    LazyVStack(alignment: .leading) {
-                        ForEach(Array(tracker.namesAndValues.keys), id: \.self) {
-                            category in
-                            Section {
-                                StatCategory(name: category, statData: tracker.namesAndValues[category]!)
-                            } header: {
-                                Text(category)
-                                    .fontWeight(.heavy)
-                            }
-                        }
+        VStack {
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(Array(tracker.namesAndValues.keys), id: \.self) {
+                        category in
                         Section {
-                            ForEach(Array(tracker.calculatedStats.keys), id: \.self) {
-                                statName in
-                                StatItem(name: statName, value: String(format: "%.2f", tracker.calculatedStats[statName]!))
-                                    .padding(.bottom)
-                            }
+                            StatCategory(name: category, statData: tracker.namesAndValues[category]!)
                         } header: {
-                            Text("Summary")
+                            Text(category)
                                 .fontWeight(.heavy)
                         }
                     }
-                    .padding([.horizontal, .bottom])
+                    Section {
+                        ForEach(Array(tracker.calculatedStats.keys), id: \.self) {
+                            statName in
+                            StatItem(name: statName, value: String(format: "%.2f", tracker.calculatedStats[statName]!))
+                                .padding(.bottom)
+                        }
+                    } header: {
+                        Text("Summary")
+                            .fontWeight(.heavy)
+                    }
                 }
-                .navigationTitle("Statistics")
-                
+                .padding([.horizontal, .bottom])
+            }
+            
+            AdaptiveVerticalView {
                 CustomNavButton(text: "BACK", action: {
                     showingStats = false
                 })
@@ -97,5 +96,8 @@ struct StatView_Previews: PreviewProvider {
     @State static var showing = true
     static var previews: some View {
         StatView(tracker: $tracker, showingStats: $showing)
+        
+        StatView(tracker: $tracker, showingStats: $showing)
+            .previewInterfaceOrientation(.landscapeRight)
     }
 }
